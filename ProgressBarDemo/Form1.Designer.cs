903209ca10178a1e9b7499c51846da5c5730b373
LogoFun13-YT <logofun13@gmail.com>
2022-11-20 15:43:55 -0500

    Update

--- ProgressBarDemo/Form1.Designer.cs
+++ ProgressBarDemo/Form1.Designer.cs
@@ -34,15 +34,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(12, 12);
-            this.progressBar1.Maximum = 200;
+            this.progressBar1.Maximum = 500;
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(557, 23);
+            this.progressBar1.Size = new System.Drawing.Size(660, 23);
             this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar1.TabIndex = 0;
             this.progressBar1.Value = 15;
@@ -50,20 +51,20 @@
             // trackBar1
             // 
             this.trackBar1.Location = new System.Drawing.Point(12, 41);
-            this.trackBar1.Maximum = 200;
+            this.trackBar1.Maximum = 500;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(557, 45);
+            this.trackBar1.Size = new System.Drawing.Size(660, 45);
             this.trackBar1.TabIndex = 1;
-            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.TickFrequency = 10;
             this.trackBar1.Value = 15;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(421, 92);
+            this.button1.Location = new System.Drawing.Point(409, 89);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 29);
+            this.button1.Size = new System.Drawing.Size(199, 38);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Change Value";
+            this.button1.Text = "Change Progress Bar Value";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button1_Click);
@@ -81,20 +82,32 @@
             this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Aqua;
             this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(19, 59);
             this.linkLabel1.LinkColor = System.Drawing.Color.Blue;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 107);
+            this.linkLabel1.Location = new System.Drawing.Point(12, 108);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(403, 23);
+            this.linkLabel1.Size = new System.Drawing.Size(403, 19);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Want to edit this? It\'s open sourced, so put a fork in it! (Fork it on Github)";
             this.linkLabel1.UseCompatibleTextRendering = true;
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(614, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Help";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 131);
+            this.ClientSize = new System.Drawing.Size(684, 131);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
@@ -143,5 +156,6 @@
         private TrackBar trackBar1;
         private Label label1;
         private LinkLabel linkLabel1;
+        private Button button2;
     }
 }
--- ProgressBarDemo/Form1.cs
+++ ProgressBarDemo/Form1.cs
@@ -12,11 +12,6 @@ namespace ProgressBarDemo
             UpdateProgressBar();
         }
 
-        private void Button1_Click2(object sender, MouseEventArgs e)
-        {
-            UpdateProgressBar();
-        }
-
         private void LinkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
         {
             try
@@ -27,6 +22,11 @@ namespace ProgressBarDemo
             {
                 MessageBox.Show($"An error occured while trying to open the link.\r\n\r\nIf this is the first time you are seeing this error, try clicking on the link again.\r\n\r\nIf you continue to keep getting this message, try one of the following options:\r\n* If the program is running in a partial-trust environment (like on a shared drive), the JIT compiler will fail upon calling the VisitLink method. If that is the case, copy the program to a local drive, and run the program from that drive.\r\n* Check your system or anti-virus settings to make sure that your internet browser isn't blocked. If it is, remove or white-list that program.\r\n* If all else fails, open the following URL in your internet browser:\r\n      github.com/LogoFun13-YT/ProgressBarDemo\r\n\r\nError details: {ex}", "Something unexpected happened", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Help for the program, Progress Bar Demo Program\r\n\r\nThis program was designed for you to have a progress bar that you can control.\r\n\r\nTo change the current value of the progress bar, Click and Drag the track bar to the value you want to set it to, then click on the button labeled \"Change Progress Bar Value\" and the progress bar will update with the new value you have set on the track bar.\r\n\r\nIf you want to play around with the source code, click the link to visit the GitHub repository.", "Help - Progress Bar Demo Program", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
